b271d18e0afbf69421a53716dbe8d8d09c051303
coddakzzi <cheon9482@gmail.com>
2026-07-18 17:08:33 +0900

    Basic Setting and Player Move
    프리팹 폴더랑 스크립트 폴더 만들었고 플레이어 움직이는거 만들었어요

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+//This is made by Bobsi Unity - Youtube
+public class PlayerController : MonoBehaviour
+{
+    [Header("Base setup")]
+    public float walkingSpeed = 7.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [SerializeField]
+    private float cameraYOffset = 0.4f;
+    private Camera playerCamera;
+
+
+    public void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        playerCamera = Camera.main;
+        playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y + cameraYOffset, transform.position.z);
+        playerCamera.transform.SetParent(transform);
+    }
+
+    void Update()
+    {
+        // We are grounded, so recalculate move direction based on axis
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = walkingSpeed *Input.GetAxis("Vertical");
+        float curSpeedY = walkingSpeed * Input.GetAxis("Horizontal");
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player and Camera rotation
+        if (playerCamera != null)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
